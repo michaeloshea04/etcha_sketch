31e789bd986ada761a49a835e32683c8b311c2ed
michaeloshea04 <michael.oshea1@jcu.edu.au>
2023-07-21 16:29:53 +0100

    Add more squares This is more detail of what I did

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function createGrid() {
     const gridContainer = document.getElementById('grid-container');
 
-    for (let i = 0; i < 8 * 8; i++) {
+    for (let i = 0; i < 16 * 16; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         gridContainer.appendChild(square);
